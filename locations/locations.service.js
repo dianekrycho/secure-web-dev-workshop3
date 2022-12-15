112978dcdb6e8b80aaf057303021a5b6b497d1a1
dianekrycho <85934723+dianekrych@users.noreply.github.com>
2022-12-15 16:37:49 +0100

    rajout try catch

--- locations/locations.service.js
+++ locations/locations.service.js
@@ -7,25 +7,49 @@ function findAll () {
 module.exports.findAll = findAll
 
 async function query_id(id){
-	return (await Location.findById(id))
+	try {
+		return (await Location.findById(id))
+	}
+	catch (e){
+		console.error(e)
+		return null
+	}
 }
 module.exports.query_id = query_id
 
 
 async function delete_id(id){
-	Location.findByIdAndDelete(id)
-	return("Supprimé")
+	try {
+		Location.findByIdAndDelete(id)
+		return("Supprimé")
+	}
+	catch (e){
+		console.error(e)
+		return null
+	}
 }
 module.exports.delete_id = delete_id
 
 async function add_loc(loc){
-	const location = new Location(loc)
-	return location.save()
+	try {
+		const location = new Location(loc)
+		return location.save()
+	}
+	catch (e){
+		console.error(e)
+		return null
+	}
 }
 module.exports.add_loc = add_loc
 
 async function modify_loc(body){
-	await Location.findByIdAndUpdate({ _id: body._id},body);
-	return ("modifié" + body)
+	try {
+		await Location.findByIdAndUpdate({ _id: body._id},body);
+		return ("modifié" + body)
+	}
+	catch (e){
+		console.error(e)
+		return null
+	}
 }
 module.exports.modify_loc = modify_loc
